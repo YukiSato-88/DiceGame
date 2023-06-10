017dcce0913d498a13d1b279f7a2f8c1a9073ec9
Yuki Sato <s1280089@u-aizu.ac.jp>
2023-06-10 18:54:24 +0900

    Greetings added

--- DiceGame.c
+++ DiceGame.c
@@ -4,12 +4,17 @@
 
 int main(){
   int a, b, sum;
+  char name[15];
 
   srand((unsigned int)time(NULL));
   a = rand() % 6 + 1;
   b = rand() % 6 + 1;
   sum = a + b;
 
+  printf("What your name?\n> ");
+  scanf("%s", name);
+  printf("Hello, %s!\n", name);
+  
   printf("Rolling dice...\n");
   printf("Die 1: %d\n", a);
   printf("Die 2: %d\n", b);
